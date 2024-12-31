3dd174e1dd49829bc340292cc4c519dd066c7646
NEVIL77 <patelnevil778@gmail.com>
2025-01-01 00:25:13 +0530

    naking Component Searching Atom Component

--- src/component/SearchAtom.jsx
+++ src/component/SearchAtom.jsx
@@ -1,8 +1,28 @@
+import SearchInputAtom from "./searchAtom/SearchInputAtom"
+
 const SearchAtom = ()=>{
+
+    const data = [
+        {
+            name : 'Apple',
+            email : "apple@gmail.com"
+        },
+        {
+            name : 'Banana',
+            email : "Banana@gmail.com"
+        },
+        {
+            name : 'Cucumber',
+            email : "Cucumber@gmail.com"
+        },
+    ]
     return (
         <>
-        
+            <SearchInputAtom/>
         </>
     )
 }
 export default SearchAtom
+
+// data in this component 
+// searching in searching Component 
