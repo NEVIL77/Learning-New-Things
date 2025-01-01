10bb10b3e9910e422322715060eedaa7cd00ed5a
NEVIL77 <patelnevil778@gmail.com>
2025-01-02 00:58:04 +0530

    color update and state
    onClick color chnages and and maintaing the state of the variable

--- src/component/matrixColor/MatrixColor.jsx
+++ src/component/matrixColor/MatrixColor.jsx
@@ -1,17 +1,47 @@
+import { useEffect, useState } from "react"
+
 const MatrixColor = () => {
 
-    const renderBox = () => {
+    
+    const renderBox = (isState = false) => {
+        const boxAofObj = []
         const box = [0, 1, 2].map((i) => {
             return [0, 1, 2].map((j) => {
-
+                
                 if (!(i == 1 && j > 0)) {
-                    return <div className="box" key={`${i}${j}`}></div>
+                    
+                    if(isState){
+                        return boxAofObj.push( { i : i,j : j , isChecked : false , order : null } )
+                    }
+                    return <div className={`box ${boxState.find((obj)=> obj.i==i && obj.j==j ).isChecked ? 'bg-green-500' : ''}`} key={`${i}${j}`} onClick={()=>handleClick(i,j)}></div>
                 }
                 return <div></div>
-
+                
             })
         })
+
+        if(isState){
+            return boxAofObj
+        }
         return box
+    }
+    
+    const [boxState,setBoxState] = useState(renderBox(true))
+    const [order,setOrder] = useState(1)
+    useEffect(()=>{
+        console.log(">>boxState",boxState)
+    },[boxState])
+
+    const isEveryGreen = boxState.every((obj)=> obj.isChecked == true)
+
+    const handleClick = (i,j)=>{
+        console.log(" >>" , i , j )
+        const click = boxState.find((obj)=> obj.i == i && obj.j == j)
+        if(click.isChecked == false){
+            click.isChecked = true
+            click.order = order
+            setOrder((order)=> order+1)
+        }
     }
 
     return (
@@ -28,7 +58,8 @@ export default MatrixColor
 // Approach 
 // 1. Create a function that returns an array of arrays and render the boxes
 // 2. make box green on click of the box
-// 3. we have to rember the order of the click and maintaing the click state  
+// 3. making box state 
+// 4. we have to rember the order of the click and maintaing the click state  
 
 
 // question 
