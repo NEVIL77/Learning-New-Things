0116465a64fba93d1279d48c7c0a9c0096960647
NEVIL77 <patelnevil778@gmail.com>
2025-01-05 21:43:15 +0530

    Folder nested and all

---
+++ src/component/folder/FolderStructure.jsx
@@ -0,0 +1,35 @@
+import { data } from "autoprefixer"
+import { useState } from "react"
+import "./style.css";
+
+const Folder = ({ explorer }) => {
+    const [expane, setExpane] = useState(false);
+
+    if (explorer.isFolder) {
+        return (
+            <div className="container">
+                <div className="folder" onClick={() => setExpane(!expane)}>
+                    <span>{expane ? "📂" : "📁"} {explorer.name}</span>
+                </div>
+
+                <div className="items" style={{ display: expane ? "block" : "none" }}>
+                    {explorer.items?.map((exp) => (
+                        <Folder explorer={exp} key={exp.id} />
+                    ))}
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <span className="file">📃 {explorer.name}</span>
+        );
+    }
+};
+
+export default Folder;
+
+
+// STEPs
+
+// 1 render the first level of the data
+// 2 on Click do hide and show 
