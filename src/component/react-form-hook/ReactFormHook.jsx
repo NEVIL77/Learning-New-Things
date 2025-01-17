c284f66ecb1f17066333e188b5ef8a217c3c907c
NEVIL77 <patelnevil778@gmail.com>
2025-01-18 01:36:39 +0530

    preventing multiple sub and error handling

--- src/component/react-form-hook/ReactFormHook.jsx
+++ src/component/react-form-hook/ReactFormHook.jsx
@@ -6,7 +6,7 @@ const ReactFormHook = ()=>{
         register,
         handleSubmit,
         watch,
-        formState: { errors },
+        formState: { errors ,isSubmitting },
     } = useForm()
     
     const handleSub = (data)=>{
@@ -18,18 +18,25 @@ const ReactFormHook = ()=>{
             hi react hook
             <form onSubmit={handleSubmit(handleSub)}> 
                 <div>
-                    <label>First Name</label>
-                    <input  {...register('first_name')} />
+                    <label>First Name :- </label>
+                    <input  {...register('first_name',
+                        {
+                            required:true,
+                            minLength : { value : 3 , message : "3 thi vadhare"}
+                        })} 
+                    />
+                    { errors.first_name && <p>{errors.first_name.message}</p>}
                 </div>
                 <div>
-                    <label>Middle Name</label>
-                    <input  {...register('middle_name')}/>
+                    <label>Middle Name :- </label>
+                    <input  {...register('middle_name',{required:true})}/>
                 </div>
                 <div>
-                    <label>Last Name</label>
+                    <label>Last Name :- </label>
                     <input  {...register('last_name')}/>
                 </div>
-                <button type="submit">Submit</button>
+                {/* <button type="submit"> Submit</button> */}
+                <input type="submit" disabled={isSubmitting} />
             </form>
         </div>
     )
